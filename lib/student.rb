10050c50d285d8361c69a05b1cd3b2dfa3546a25
Riley Moore <rilemoore@gmail.com>
2019-11-08 20:14:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,8 @@ class Student
   attr_reader :id
 
   def initialize(:name, :grade, id: nil)
-    
+    @name = name
+    @grade = grade
   end
 
   # Remember, you can access your database connection anywhere in this class
