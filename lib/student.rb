5e4632a5ba6ff26758cf56f53ce5bde8627ff2b3
Riley Moore <rilemoore@gmail.com>
2019-11-08 20:20:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   end
   
   def self.create_table
-    sql = "CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER;"
+    sql = "CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER);"
     DB[:conn].execute(sql) 
   end
 
