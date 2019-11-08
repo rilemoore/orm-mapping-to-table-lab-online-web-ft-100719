78bb691b0373317d8d826e197119929be2fd3a80
Riley Moore <rilemoore@gmail.com>
2019-11-08 20:26:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,10 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = ""
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
